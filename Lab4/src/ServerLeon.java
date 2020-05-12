b4bbf7e97d968767296103587d29cd08d60bd0a4
nt2743 <s0568727@htw-berlin.de>
2020-05-12 19:07:21 +0200

    threads and bug with quit

--- Lab4/src/ServerLeon.java
+++ Lab4/src/ServerLeon.java
@@ -4,6 +4,7 @@ import java.net.Socket;
 
 public class ServerLeon {
     public static void main(String[] args) throws IOException {
+        //Step 1
         ServerSocket server = new ServerSocket(3000);
         Socket client = server.accept();
 
@@ -11,21 +12,61 @@ public class ServerLeon {
 
         PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
 
-        InputStreamReader input  = new InputStreamReader(client.getInputStream());
+        InputStreamReader input = new InputStreamReader(client.getInputStream());
         BufferedReader reader = new BufferedReader(input);
         BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 
-        try {
+        //Step 2
+        /*try {
             while (true) {
                 String serverMessage = keyboard.readLine();
                 writer.println(serverMessage);
                 String clientResponse = reader.readLine();
                 System.out.println("Client: " + clientResponse);
-                }
-        }
-        finally {
+            }
+        } finally {
             writer.close();
             reader.close();
         }
+        */
+
+        //Step 3
+        Thread send = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        String serverMessage = keyboard.readLine();
+                        writer.println(serverMessage);
+                        if (serverMessage.equals("quit")) {
+                            client.close();
+                            break;
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        send.start();
+
+        Thread read = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        String clientResponse = reader.readLine();
+                        System.out.println("Client: " + clientResponse);
+                        if (clientResponse.equals("quit")) {
+                            client.close();
+                            break;
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        read.start();
     }
 }
