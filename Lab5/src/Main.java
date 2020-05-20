2846cdb52b18fc60ef9b3c967356c579d11af5d7
s0572665 <mirijam.trippel@student.htw-berlin.de>
2020-05-20 18:02:46 +0200

    update fast fertig, nur noch gemeinsam probieren + primzahlen + report

--- Lab5/src/Main.java
+++ Lab5/src/Main.java
@@ -11,88 +11,104 @@ public class Main {
 
     public static void main(String[] args) {
         int n = 50;
-        System.out.println(fragment7(n));
+        System.out.println(fragment1(n));
+        System.out.println(timerResult);
 
     }
 
     // https://www.baeldung.com/java-algorithm-complexity
 
 
-    // Linear Time Algorithms – O(n) = 50
+    static double timerStart;
+    static double timerEnd;
+    static double timerResult;
+
+    // Linear Time Algorithms – O(n) = 50 // T = ungefähr 2900.0
     public static int fragment1(int n){
         int sum = 0;
-
+        timerStart = System.nanoTime();
         for ( int i = 0; i < n; i ++)
             sum++;
-
+        timerEnd = System.nanoTime();
+        timerResult = timerEnd - timerStart;
         return sum;
+
     }
+
+
+
     // O(n^2) = 2500
     public static int fragment2(int n){
         int sum = 0;
-
+        timerStart = System.nanoTime();
         for ( int i = 0; i < n; i ++)
             for ( int j = 0; j < n; j ++)
                 sum++;
-
+        timerEnd = System.nanoTime();
+        timerResult = timerEnd - timerStart;
         return sum;
     }
 
     // ??? = 1275 wie kann das sein denn wir haben doch die selbe größe als i
     public static int fragment3(int n){
         int sum = 0;
-
+        timerStart = System.nanoTime();
         for ( int i = 0; i < n; i ++)
             for ( int j = i; j < n; j ++)
                 sum++;
-
+        timerEnd = System.nanoTime();
+        timerResult = timerEnd - timerStart;
         return sum;
     }
 
-    // Linear Time Algorithms – O(2n) = 100
+    // Linear Time Algorithms – O(n) = 100
     public static int fragment4(int n){
         int sum = 0;
-
+        timerStart = System.nanoTime();
         for ( int i = 0; i < n; i ++)
             sum ++;
         for ( int j = 0; j < n; j ++)
             sum ++;
-
+        timerEnd = System.nanoTime();
+        timerResult = timerEnd - timerStart;
         return sum;
     }
 
     // O(n^3) = 125000
     public static int fragment5(int n){
         int sum = 0;
-
+        timerStart = System.nanoTime();
         for ( int i = 0; i < n; i ++)
             for ( int j = 0; j < n*n; j ++)
                 sum++;
-
+        timerEnd = System.nanoTime();
+        timerResult = timerEnd - timerStart;
         return sum;
     }
 
     // ??? = 1225
     public static int fragment6(int n){
         int sum = 0;
-
+        timerStart = System.nanoTime();
         for ( int i = 0; i < n; i ++)
             for ( int j = 0; j < i; j ++)
                 sum++;
-
+        timerEnd = System.nanoTime();
+        timerResult = timerEnd - timerStart;
         return sum;
     }
 
     // ??? = 13996133
     public static int fragment7(int n){
         int sum = 0;
-
+        timerStart = System.nanoTime();
         for ( int i = 1; i < n; i ++)
             for ( int j = 0; j < n*n; j ++)
                 if (j % i == 0)
                     for (int k = 0; k < j; k++)
                         sum++;
-
+        timerEnd = System.nanoTime();
+        timerResult = timerEnd - timerStart;
         return sum;
     }
 
